ee96e04162ec72803cea239ef3d2fc57227da88b
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-04-28 18:36:06 +0200

    added first example for image classification

---
+++ ml5 examples/image_classification/sketch.js
@@ -0,0 +1,30 @@
+let mobilenet;
+let puffin;
+
+function preload() {
+  puffin = loadImage( 'assets/puffin.jpg' );
+}
+
+function setup() {
+  createCanvas( 400, 400 );
+  background( 100 );
+  //load mobilenet models and invoke the modelReady callback when loaded
+  mobilenet = ml5.imageClassifier( 'MobileNet', modelReady );
+  image( puffin, 0, 0, width );
+}
+
+function modelReady() {
+  console.log( "model is ready" );
+  //use the model to predict what's the image looks like
+  //the gotResults() callback will be invoked once the image is processed
+  mobilenet.predict( puffin, gotResults );
+}
+
+// by default you want error as a first parameter of the callback.
+function gotResults( error, results ) {
+  if ( error ) {
+    console.error( error );
+  } else {
+    console.log( results );
+  }
+}
